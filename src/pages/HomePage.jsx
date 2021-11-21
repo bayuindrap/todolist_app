e3931afcc02e4077bf125a74372e571940e4cca7
bayuindrap <indraputrabayu@gmail.com>
2021-11-21 21:11:23 +0700

    btn submit todolist

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+
+        }
+    }
+    render() { 
+        return (  
+        //     <div>
+
+        //     <div>
+        //     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        //     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span className="navbar-toggler-icon"></span>
+        //     </button>
+        //     <a className="navbar-brand" href="#!">To Do App</a>
+
+        //     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        //         <ul className="navbar-nav mr-auto mt-2 mt-md-0">
+        //         <li className="nav-item active">
+        //             <a className="nav-link" href="#!">Home<span class="sr-only">(current)</span></a>
+        //         </li>
+        //         <li className="nav-item">
+        //             <a className="nav-link" href="#!">Link</a>
+        //         </li>
+        //         </ul>
+        //     </div>
+        //     </nav>
+        //     </div>
+
+
+        //     <div>
+        //         <p>Date</p>
+        //         <input id="date" type="text" ref="date"/>
+        //         <p>To Do</p>
+        //         <input id="todo" type="text" ref="todo"/>
+        //         <p>Location</p>
+        //         <input id="location" type="text" ref="location"/>
+        //         <p>Note</p>
+        //         <input id="note" type="text" ref="note" style={{ height: '30vh' }}/>
+                
+
+        //     </div>
+
+        //     <div>
+        //     <button type="button" className="btn btn-primary">Submit</button>
+        //     </div>
+
+        //     <div>
+        //                 <table class="table">
+        //     <thead>
+        //         <tr>
+        //         <th scope="col">No</th>
+        //         <th scope="col">Date</th>
+        //         <th scope="col">To Do</th>
+        //         <th scope="col">Locaton</th>
+        //         <th scope="col">Note</th>
+        //         <th scope="col">Status</th>
+        //         <th scope="col">Action</th>
+        //         </tr>
+        //     </thead>
+        //     <tbody>
+        //         <tr>
+        //         <th scope="row">1</th>
+        //         <td></td>
+        //         <td></td>
+        //         <td></td>
+        //         </tr>
+        //         <tr>
+        //         <th scope="row">2</th>
+        //         <td></td>
+        //         <td></td>
+        //         <td></td>
+        //         </tr>
+        //         <tr>
+        //         <th scope="row">3</th>
+        //         <td></td>
+        //         <td></td>
+        //         <td></td>
+        //         </tr>
+        //     </tbody>
+        //     </table>
+        //     </div>
+
+
+
+
+
+
+
+
+        // </div>
+        );
+    }
+}
+ 
+export default HomePage;
